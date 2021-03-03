5d44cc9a9456ca2c5899c276d16a0b78aeda1bb3
Christine Seeman <christine.seeman@getflywheel.com>
2021-03-03 13:45:32 -0600

    I gotta gear

---
+++ gear.rb
@@ -0,0 +1,9 @@
+class Gear
+  attr_reader :chainring, :cog, :rim, :tire
+  def initialize(chainring, cog, rim, tire)
+    @chainring = chainring
+    @cog = cog
+    @rim = rim
+    @tire = tire
+  end
+end
