169282e659dfb20a150de8a243c7eb23582667d7
Savannah Albanez <savannah@getflywheel.com>
2021-03-04 14:56:08 -0600

    gear and wheel

--- gear.rb
+++ gear.rb
@@ -1,9 +1,22 @@
 class Gear
-  attr_reader :chainring, :cog, :rim, :tire
-  def initialize(chainring, cog, rim, tire)
+  attr_reader :chainring, :cog, :wheel
+  def initialize(chainring, cog, wheel = nil)
     @chainring = chainring
     @cog = cog
-    @rim = rim
-    @tire = tire
+    @wheel = wheel
+  end
+
+  def ratio
+    chainring/cog.to_f
+  end
+
+  def gear_inches
+    ratio*wheel.diameter
   end
 end
+
+
+require "./wheel.rb"
+@wheel = Wheel.new(26, 1.5)
+@gear = Gear.new(52, 11, @wheel)
+puts @gear.gear_inches
